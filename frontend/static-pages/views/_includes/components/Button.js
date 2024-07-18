a7d3eed8141803eed97f513794950a46b5d739b6
Max Nankivell <maxnankivell1@gmail.com>
2024-07-18 21:08:13 +1200

    make nav and add variables

--- frontend/static-pages/views/_includes/components/Button.js
+++ frontend/static-pages/views/_includes/components/Button.js
@@ -1,8 +1,8 @@
 const { html } = require("common-tags");
 
-function Button({ text }) {
+function Button({ text, type }) {
   return html`
-    <button class="button">
+    <button class="button ${type}">
       <div class="button-inner">${text}</div>
     </button>
   `;
