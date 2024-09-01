21a2b9de6b1d5228e95ff91f3b23c69507dd7ad4
Max Nankivell <maxnankivell1@gmail.com>
2024-09-01 17:41:53 +1200

    add nav bar to root layout

--- frontend/quiz-app/src/pages/RootLayout.tsx
+++ frontend/quiz-app/src/pages/RootLayout.tsx
@@ -1,9 +1,32 @@
 import { Outlet } from "react-router-dom";
+import AvatorImage from "../assets/avatar.png";
+import { Paper } from "@mui/material";
 
 function RootLayout() {
   return (
-    <div>
-      <div>Layout</div>
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "grid",
+        gridTemplateColumns: "1fr",
+        gridTemplateRows: "80px 1fr",
+      }}
+    >
+      <Paper square elevation={1}>
+        <nav
+          style={{
+            padding: "8px",
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <img src="/quizzle-logo.svg" alt="logo" />
+          <img src={AvatorImage} alt="avatar" />
+        </nav>
+      </Paper>
       <Outlet />
     </div>
   );
