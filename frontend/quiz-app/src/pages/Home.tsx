1db2b803a572433e697d2d7715bb6099a3faf4e2
max nankivell <max.strutfit@outlook.com>
2025-02-17 12:25:53 +1300

    add buttons to home screen

--- frontend/quiz-app/src/pages/Home.tsx
+++ frontend/quiz-app/src/pages/Home.tsx
@@ -1,5 +1,29 @@
+import { Box, Button } from "@mui/material";
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", gap: 3, p: 3 }}>
+      <Button
+        variant="contained"
+        size="large"
+        onClick={() => {
+          // TODO: Add navigation to view quizzes
+        }}
+      >
+        View Quizzes
+      </Button>
+
+      <Button
+        variant="contained"
+        size="large"
+        onClick={() => {
+          // TODO: Add navigation to create quiz
+        }}
+      >
+        Create Quiz
+      </Button>
+    </Box>
+  );
 }
 
 export default Home;
